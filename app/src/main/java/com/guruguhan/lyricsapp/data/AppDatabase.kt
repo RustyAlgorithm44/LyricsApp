62f904d9a149108752b09d2bce73c4625c834391
Guruguhan <guruguhan23@gmail.com>
2025-12-17 11:50:49 +0530

    Add Room AppDatabase

---
+++ app/src/main/java/com/guruguhan/lyricsapp/data/AppDatabase.kt
@@ -0,0 +1,31 @@
+package com.guruguhan.lyricsapp.data
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(
+    entities = [Song::class],
+    version = 1,
+    exportSchema = false
+)
+abstract class AppDatabase : RoomDatabase() {
+
+    abstract fun songDao(): SongDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: AppDatabase? = null
+
+        fun getDatabase(context: Context): AppDatabase {
+            return INSTANCE ?: synchronized(this) {
+                Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java,
+                    "lyrics_db"
+                ).build().also { INSTANCE = it }
+            }
+        }
+    }
+}
