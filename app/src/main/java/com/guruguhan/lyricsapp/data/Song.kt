c68e5fa82e2812d607e62f9055967f8bd1a12f1e
Guruguhan <guruguhan23@gmail.com>
2025-12-17 11:39:46 +0530

    Add Song entity

---
+++ app/src/main/java/com/guruguhan/lyricsapp/data/Song.kt
@@ -0,0 +1,14 @@
+package com.guruguhan.lyricsapp.data
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "songs")
+data class Song(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val title: String,
+    val artist: String,
+    val category: String,
+    val lyrics: String
+)
