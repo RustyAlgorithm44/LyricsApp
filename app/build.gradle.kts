212d080d75eb876b04abb8fdd9e72a6d940c007a
Guruguhan <guruguhan23@gmail.com>
2025-12-24 14:04:51 +0530

    Updated version for addition of language feature

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.guruguhan.lyricsapp"
         minSdk = 24
         targetSdk = 36
-        versionCode = 4
-        versionName = "1.2.0"
+        versionCode = 5
+        versionName = "1.2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
